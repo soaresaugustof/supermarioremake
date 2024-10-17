4c0d2dea062d663388d65f0251d41d6d45c8d58a
soaresaugustof <soaresaugusto@edu.unifor.br>
2024-10-17 07:08:38 -0300

    salvando progresso

---
+++ Scripts/start_screen.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+@onready var timer = $Timer
+@onready var texture_rect = $MarginContainer/VBoxContainer/TextureRect
+
+
+var control_array = []
+
+func _ready():
+	control_array = [texture_rect]
+	for control in control_array:
+		(control as Control).visible = false
+
+
+func _load_game():
+	get_tree().change_scene("res://Scenes/main.tscn")
+
+func _show_next_control():
+	var control = control_array.pop_front() as Control
+	if control != null:
+		control.visible = true
+	else:
+		timer.stop()
+		timer.queue_free()
